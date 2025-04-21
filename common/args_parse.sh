64b623f609267cd32a761cd55cf88f3e78b17339
Laurent Koné <lkone@Laurents-MacBook-Pro.local>
2025-04-21 22:10:57 +0200

    feat: improve arg parse

--- common/args_parse.sh
+++ common/args_parse.sh
@@ -19,6 +19,7 @@ args_parse() {
     #   - Positional argument(s) must be declared in top of ARG_OPTS
     #   - Named argument can be used with syntax: --arg value or --arg-value
     #   - Script add --help argument so don't add it in ARG_OPTS
+    #   - Use ARG_OPTS_CHECK=0, to skip syntax check of ARG_OPTS
     #
     # Examples:
     #   source ./args.sh
@@ -35,32 +36,31 @@ args_parse() {
     #   my-script.sh -h
     #   my-script.sh --help
 
+    local args=""
     local arg_item_sep='#'
-    local max_width=100
+    local max_width=120
+    local no_pos_arg=0
 
     _format_header() {
         local header=$@
-        echo $header
-        echo $header | sed -r 's#[A-Z0-9\-\_ ]#-#gi'
+        printf "\n%s\n" "$header"
+        printf "%s\n" "$header" | sed 's/[[:alnum:]_ -]/-/g'
     }
 
     _format_text() {
         local header=$1
         shift 1
         local text=$@
-        echo
-        _format_header $header
-        echo -e "$text" | fold -s -w $max_width
-        echo
+        _format_header "$header"
+        printf "%s\n\n" "$text" | fold -s -w $max_width
     }
 
     _format_help() {
         local header=$1
         shift 1
         local help_text=$@
-        echo
-        _format_header $header
-        echo "$help_text" | awk -F';;' 'BEGIN { maxd = 0 } {
+        _format_header "$header"
+        awk -F';;' 'BEGIN { maxd = 0 } {
             for (i = 1; i <= NF; i++) {
                 split($i, parts, ":::")
                 len = length(parts[1])
@@ -71,21 +71,20 @@ args_parse() {
                 split(data[i], parts, ":::")
                 if (parts[1]) printf "%-*s : %s\n", maxd, parts[1], parts[2]
             }
-        }'
-        echo
+        }' <<< $help_text
+        printf "\n"
     }
 
     _err_msg() {
         local msg=$@
-        local help_msg="[Type --help for more details]\n\nUsage: $(_print_usage 1)"
-        echo -e "[ARG] ERROR: ${msg} ${help_msg}" >&2
+        local usage=$(_print_usage 1 | fold -s -w $max_width)
+        printf "[ARG] ERROR: %s [Type --help for more details]\n\nUsage: %s\n" "${msg}" "${usage}" >&2
         exit 1
-
     }
 
     _conf_err_msg() {
         local msg=$@
-        echo -e "[ARG:CONFIG] ERROR: ${msg}" >&2
+        printf "[ARG:CONFIG] ERROR: %s\n" "${msg}" >&2
         exit 11
     }
 
@@ -102,9 +101,9 @@ args_parse() {
             fmt_arg="$arg"
             fmt_description=$description
             if [[ $arg_type = "n" ]]; then
-                [[ -n $choices ]]  && fmt_arg+=" {${choices}}" || fmt_arg+=" {${var_name}}"
+                [[ -n $choices ]] && fmt_arg+=" {${choices}}" || fmt_arg+=" {${var_name}}"
             elif [[ $arg_type = "p" ]]; then
-                [[ -n $choices ]]  && fmt_arg+=":${choices}"
+                [[ -n $choices ]] && fmt_arg+=":${choices}"
                 fmt_arg="<${fmt_arg}>"
             fi
             [[ -n $default_value ]] && fmt_description+=" [Default: '$default_value']"
@@ -117,13 +116,14 @@ args_parse() {
             fi
             # Add help usage
             [[ $is_required != 1 ]] && fmt_arg="(${fmt_arg})"
-            [[ $arg_type = "p" || $is_required = 1 ]] && help_usage_required+=" $fmt_arg" || help_usage_additional+=" $fmt_arg"
+            usage_fmt_arg="${fmt_arg// /=}"
+            [[ $arg_type = "p" || $is_required = 1 ]] && help_usage_required+=" $usage_fmt_arg" || help_usage_additional+=" $usage_fmt_arg"
         done < <(printf '%s\n' "${ARG_OPTS[@]}")
         # Build usage
         local help_usage="$(basename $0)${help_usage_required}${help_usage_additional}"
-        [[ $only_usage = 1 ]] && { echo "$help_usage"; return; }
+        [[ $only_usage = 1 ]] && { printf "%s\n" "$help_usage"; return; }
         # Add help args
-        help_additional+="--help|-h:::Help"
+        help_additional+="--help|-h:::Display Help"
         [[ -n $ARG_DESCRIPTION ]] && _format_text "Description" "$ARG_DESCRIPTION"
         _format_text "Usage" "$help_usage"
         [[ -n $help_positional ]] && _format_help "Positional Arguments" $help_positional
@@ -131,60 +131,65 @@ args_parse() {
         _format_help "Additional Arguments" $help_additional
     }
 
-    local args=""
-    local no_pos_arg=0
-    # Declare variables and check arguments syntax
-    while IFS="$arg_item_sep" read arg _ var_name arg_type is_required default_value _; do
-        [[ -z $arg ]] && _conf_err_msg "Arg can't be empty."
-        declare -p "$var_name" &> /dev/null && _conf_err_msg "Variable '$var_name' already declared"
-        echo "$args" | grep -qE -- "$arg" && _conf_err_msg "Argument '$arg' already used"
-        ! echo $arg_type | grep -qE '^[nbp]$' && _conf_err_msg "Bad Argument type: $arg_type (n: named, b: boolean, p: positional)"
-        ! echo $var_name | grep -iqE '^[a-z0-9\_]+$' && _conf_err_msg "Bad var_name format: $var_name (Syntax: 'VAR_NAME', 'my_var')"
-        if [[ $arg_type = "p" ]]; then
-            [[ $no_pos_arg = 1 ]] && _conf_err_msg "Positional argument must be declared before other args (named/boolean)"
-            [[ $is_required != 1 ]] && _conf_err_msg "Positional argument must be required"
-            ! echo $arg | grep -qE '^[a-z0-9\_]+$' && _conf_err_msg "Bad positional argument format: $arg (Syntax: 'arg_name')"
-        else
-            no_pos_arg=1
-            ! echo $arg | grep -qE -- '^--[a-z0-9\-]+(\|-[a-z0-9])?$' && _conf_err_msg "Bad named/boolean argument format: $arg (Syntax: '--long-arg' or '--long-arg|-s')"
-        fi
-        echo $arg_type | grep -qE '^[bp]$' && [[ -n $default_value ]] && _conf_err_msg "No default value for positional/boolean args (Set to empty)"
-        [[ $arg_type = "b" ]] && [[ -n $is_required || -n $choices ]] && _conf_err_msg "Boolean arg can't be required or have choices (Set to empty)"
-        # Get default value (if not set)
-        if [[ -z $default_value ]]; then
-            [[ $arg_type = "b" ]] && default_value=0 || default_value=""
-        fi
-        eval "$var_name='${default_value}'"
-        args+="$(echo $arg | tr '|' ' ')"
-    done < <(printf '%s\n' "${ARG_OPTS[@]}")
+    _init_opts() {
+        # Declare variables and check arguments syntax
+        while IFS="$arg_item_sep" read arg description var_name arg_type is_required default_value choices; do
+            if [[ $ARG_CHECK_OPTS = 1 ]]; then
+                [[ -z $arg || -z $description ]] && _conf_err_msg "Arg or Description can't be empty."
+                declare -p "$var_name" &> /dev/null && _conf_err_msg "Variable '$var_name' already declared"
+                [[ " ${args[*]} " == *" ${arg//|/ } "* ]] && _conf_err_msg "Argument '$arg' already used"
+                [[ ! $arg_type =~ ^[nbp]$ ]] && _conf_err_msg "Bad Argument type: $arg_type (n: named, b: boolean, p: positional)"
+                [[ ! $var_name =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]] && _conf_err_msg "Bad var_name format: $var_name (Syntax: 'VAR_NAME', 'my_var')"
+                if [[ $arg_type = "p" ]]; then
+                    [[ $no_pos_arg = 1 ]] && _conf_err_msg "Positional argument must be declared before other args (named/boolean)"
+                    [[ $is_required != 1 ]] && _conf_err_msg "Positional argument must be required"
+                    [[ ! $arg =~ ^[a-z0-9_]+$ ]] && _conf_err_msg "Bad positional argument format: $arg (Syntax: 'arg_name')"
+                else
+                    no_pos_arg=1
+                    [[ ! $arg =~ ^--[a-z0-9\-]+(\|-[a-z0-9])?$ ]] && _conf_err_msg "Bad named/boolean argument format: $arg (Syntax: '--long-arg' or '--long-arg|-s')"
+                fi
+                [[ $arg_type =~ ^[bp]$ && -n $default_value ]] && _conf_err_msg "No default value for positional/boolean args (Set to empty)"
+                [[ $arg_type = "b" ]] && [[ -n $is_required || -n $choices ]] && _conf_err_msg "Boolean arg can't be required or have choices (Set to empty)"
+                [[ -n $default_value && -n $choices ]] && [[ ! " ${choices//|/ } " =~ " $default_value " ]] && _conf_err_msg "Default value for '$arg' must be present in: $choices"
+            fi
+            # Get default value (if not set)
+            if [[ -z $default_value ]]; then
+                [[ $arg_type = "b" ]] && default_value=0 || default_value=""
+            fi
+            eval "$var_name='${default_value}'"
+            args+=" ${arg//|/ }"
+        done < <(printf '%s\n' "${ARG_OPTS[@]}")
+    }
 
+    _init_opts
     for uidx in $(seq 1 $#); do
         cshift=0
         wegal=0
-        uarg=$1
-        uprm=$2
-        if echo $uarg | grep -qE -- "--?[a-z0-9\-]+\=.*"; then
+        if [[ $1 =~ ^--?[a-z0-9-]+=.* ]]; then
             wegal=1
-            uarg=$(echo "$1" | cut -d'=' -f1)
-            uprm=$(echo "$1" | cut -d'=' -f2-)
+            uarg="${1%%=*}"
+            uprm="${1#*=}"
+        else
+            uarg=$1
+            uprm=$2
         fi
         idx=1
         while IFS="$arg_item_sep" read arg _ var_name arg_type is_required _ choices; do
-            fmt_arg=$(echo $arg | tr '|' ' ')
-            fmt_choices=$(echo $choices | tr '|' ' ')
-            echo "--help -h" | grep -qE -- "(^| )$uarg($| )" && { _print_usage; exit 0; }
+            fmt_arg="${arg//|/ }"
+            fmt_choices="${choices//|/ }"
+            [[ " --help -h " == *" $uarg "* ]] && { _print_usage; exit 0; }
             if [[ $arg_type = "p" ]] && [[ $idx = $uidx ]]; then
-                [[ -z $uarg ]] || echo "$args" | grep -qE -- "(^| )$uarg($| )" && _err_msg "Positional arg needed: '$var_name'"
-                [[ -n $choices ]] && ! echo "$fmt_choices" | grep -qE -- "(^| )$uarg($| )" && _err_msg "Bad value for positional arg: $var_name"
+                [[ " $args " == *" $uarg "* ]] && _err_msg "Positional arg needed: '$var_name'"
+                [[ -n $choices && " $fmt_choices " != *" $uarg "* ]] && _err_msg "Bad value for positional arg: $uarg"
                 eval "$var_name='${uarg}'"
                 cshift=1
-            elif echo "$fmt_arg" | grep -qE -- "(^| )$uarg($| )"; then
+            elif [[ " $fmt_arg " == *" $uarg "* ]]; then
                 if [[ $arg_type = "b" ]]; then
                     eval "$var_name=1"
                     cshift=1
                 else
-                    [[ -z $uprm ]] || echo "$args" | grep -qE -- "(^| )$uprm($| )" && _err_msg "Param needed for '$uarg'"
-                    [[ -n $choices ]] && ! echo "$fmt_choices" | grep -qE -- "(^| )$uprm($| )" && _err_msg "Bad value for: $uarg"
+                    [[ " $args " == *" $uprm "* ]] && _err_msg "Param needed for '$uarg'"
+                    [[ -n $choices && " $fmt_choices " != *" $uprm "* ]] && _err_msg "Bad value for: $uarg"
                     eval "$var_name='${uprm}'"
                     [[ $wegal = 1 ]] && cshift=1 || cshift=2
                 fi
